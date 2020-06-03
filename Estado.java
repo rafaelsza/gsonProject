b07cb49afb1c7eeb619a05f429bd3535ff6789c8
rafaelsza <65384977+rafaelsza@users.noreply.github.com>
2020-06-03 19:35:21 -0300

    Add files via upload

---
+++ Estado.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testes;
+
+/**
+ *
+ * @author Souza
+ */
+public class Estado {
+    private int id;
+    private String sigla;
+    private String nome;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the sigla
+     */
+    public String getSigla() {
+        return sigla;
+    }
+
+    /**
+     * @param sigla the sigla to set
+     */
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ UFsJSON.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testes;
+
+import com.google.gson.Gson;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Souza
+ */
+public class UFsJSON {
+    private static final int HTTP_COD_SUCESSO = 200;
+    
+    public static void main(String[] args) throws IOException {
+//        URL url = new URL("https://servicodados.ibge.gov.br/api/v1/localidades/estados");
+//        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+//        
+//        if (con.getResponseCode() != HTTP_COD_SUCESSO) {
+//                throw new RuntimeException("HTTP error code : "+ con.getResponseCode());
+//        }
+//
+//        BufferedReader br = new BufferedReader(new InputStreamReader((con.getInputStream())));
+//        
+//        System.out.println(br);
+        
+        try {
+            String url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome";
+
+            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                System.out.println("Erro " + conn.getResponseCode() + " ao obter dados da URL " + url);
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+            String output = "";
+            String line;
+            while ((line = br.readLine()) != null) {
+                output += line;
+            }
+
+            conn.disconnect();
+            
+
+            Gson gson = new Gson();
+            Estado1 estado = gson.fromJson(new String(output.getBytes()), Estado1.class);
+
+            System.out.println(Arrays.toString(estado.getEstados()));
+
+        } catch (IOException ex) {
+            ex.toString();
+        }
+    }
+}
