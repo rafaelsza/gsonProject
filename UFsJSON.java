a2568ebad845148fe132a514e4fbd5a3a3f9cd9a
Rafael Souza <65384977+rafaelsza@users.noreply.github.com>
2020-06-04 00:20:56 -0300

    Update UFsJSON.java

--- UFsJSON.java
+++ UFsJSON.java
@@ -5,35 +5,22 @@
  */
 package testes;
 
-import com.google.gson.Gson;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  *
  * @author Souza
  */
 public class UFsJSON {
-    private static final int HTTP_COD_SUCESSO = 200;
-    
-    public static void main(String[] args) throws IOException {
-//        URL url = new URL("https://servicodados.ibge.gov.br/api/v1/localidades/estados");
-//        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-//        
-//        if (con.getResponseCode() != HTTP_COD_SUCESSO) {
-//                throw new RuntimeException("HTTP error code : "+ con.getResponseCode());
-//        }
-//
-//        BufferedReader br = new BufferedReader(new InputStreamReader((con.getInputStream())));
-//        
-//        System.out.println(br);
-        
+    public ArrayList<Estado> getUFs(){
         try {
             String url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome";
 
@@ -43,27 +30,35 @@ public class UFsJSON {
             conn.setRequestProperty("Accept", "application/json");
 
             if (conn.getResponseCode() != 200) {
-                System.out.println("Erro " + conn.getResponseCode() + " ao obter dados da URL " + url);
+                JOptionPane.showMessageDialog(null, "Erro " + conn.getResponseCode() + " ao obter dados da URL " + url, "ERROR", JOptionPane.ERROR_MESSAGE);
             }
 
             BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+            String output = br.readLine();
+            output = output.replace("[", "{ \"estados\": [");
+            output = output.replace("]", "]}");
+            
+            System.out.println(output);
+            
+            conn.disconnect();
 
-            String output = "";
-            String line;
-            while ((line = br.readLine()) != null) {
-                output += line;
+            JSONObject JsObject = new JSONObject(output);
+            JSONArray ArrayEstados = JsObject.getJSONArray("estados");
+            ArrayList<Estado> estados = new ArrayList();
+            
+            for (int i = 0; i < ArrayEstados.length(); ++i) {
+              final JSONObject JsObjectEstado = ArrayEstados.getJSONObject(i);
+              Estado estado = new Estado();
+              estado.setId(JsObjectEstado.getInt("id"));
+              estado.setSigla(JsObjectEstado.getString("sigla"));
+              estado.setNome(JsObjectEstado.getString("nome"));
+              estados.add(estado);
             }
 
-            conn.disconnect();
-            
-
-            Gson gson = new Gson();
-            Estado estado = gson.fromJson(new String(output.getBytes()), Estado.class);
-
-            System.out.println(estado.getSigla());
-
+            return estados;
         } catch (IOException ex) {
             ex.toString();
         }
+        return null;
     }
 }
